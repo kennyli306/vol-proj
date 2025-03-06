3fc1cef2d61175a24688d709e9e0f86aa6094295
kennyli306 <kennyli306@gmail.com>
2025-03-06 15:33:05 -0500

    notes

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,9 +3,13 @@ import Footer from '@app//components/Footer';
 import NavBar from '@app/components/NavBar';
 
 export default function SearchPage() {
+    // search header
+    // postings
+    // add posting
     return (
         <div className="flex flex-col min-h-screen max-w-[1280px] mx-auto">
             <NavBar />
+
             <Footer />
         </div>
     )
