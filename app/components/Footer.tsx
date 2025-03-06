c0e1a94b9a8eb6bdc1ff2878fd8497baeca7f362
kennyli306 <kennyli306@gmail.com>
2025-03-06 13:30:52 -0500

    initial page

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+
+    return (
+        <footer className="footer sm:footer-center bg-neutral text-neutral-content p-10">
+            <nav>
+                <h6 className="footer-title">Vol Project</h6>
+                <a className="link link-hover">Kenny Li</a>
+            </nav>
+        </footer>
+    );
+}
+
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+interface NavBarArgs {
+    navItems: string[];
+}
+
+export default function NavBar({ navItems }: NavBarArgs) {
+
+
+    return (
+        <div className="flex flex-col min-h-screen">
+        </div>
+    );
+}
+
