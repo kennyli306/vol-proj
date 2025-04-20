33af37987e803af1d0f9cc661fcbe02f7faa967b
kennyli306 <kennyli306@example.com>
2025-04-20 00:04:37 -0400

    removed User table

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,9 @@
-import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID!;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET!;
-
-const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
     session: {
@@ -17,34 +14,7 @@ export const authOptions: NextAuthOptions = {
             clientId: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
         }),
-    ],
-    callbacks: {
-        async signIn({ account, profile }) {
-            if (!profile?.email) {
-                throw new Error("No profile");
-            }
-
-            try {
-                await prisma.user.upsert({
-                    where: {
-                        email: profile.email,
-                    },
-                    create: {
-                        email: profile.email,
-                        name: profile.name,
-                    },
-                    update: {
-                        name: profile.name,
-                    },
-                })
-            }
-            catch (error) {
-                console.error("Error upserting user:", error);
-            }
-
-            return true;
-        }
-    }
+    ]
 }
 
 const handler = NextAuth(authOptions);
