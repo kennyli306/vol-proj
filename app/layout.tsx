8743d119d22210aaa8f9441b6ba51ca1e293519e
kennyli306 <kennyli306@example.com>
2025-04-20 00:02:12 -0400

    title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Vol Project",
+    title: "Volunteer Project",
     description: "Find volunteer opportunities near you",
 };
 
